176271d52dd69e59d37bd50c440d038bca30bcd9
Pinga <help@namingo.org>
2025-12-29 12:31:24 +0200

    Update hooks.php

--- namingo_registrar/hooks.php
+++ namingo_registrar/hooks.php
@@ -6,9 +6,9 @@ if (!defined("WHMCS")) {
 add_hook('ClientAreaFooterOutput', 1, function ($vars) {
     $html = <<<HTML
 <div class="text-center" style="margin:10px 0;">
-  <p><a href="index.php?m=registrar&page=whois">Domain Lookup</a>
+  <p><a href="index.php?m=namingo_registrar&page=whois">Domain Lookup</a>
   &nbsp;|&nbsp;
-  <a href="index.php?m=registrar&page=contact">Registrant Contact</a></p>
+  <a href="index.php?m=namingo_registrar&page=tmch">TMCH Claims Notice</a></p>
 </div>
 HTML;
     return $html;
@@ -17,7 +17,7 @@ HTML;
 add_hook('ClientAreaPrimaryNavbar', 1, function ($navbar) {
     $navbar->addChild('Domain Lookup', [
         'label' => 'Domain Lookup',
-        'uri'   => 'index.php?m=registrar&page=whois',
+        'uri'   => 'index.php?m=namingo_registrar&page=whois',
         'order' => 50,
     ]);
 });
